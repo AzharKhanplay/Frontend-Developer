f03a584e188dcc5984612ca09a082147fcc64838
AzharKhanPlay <azharkhanplay@gmail.com>
2023-02-10 00:39:50 +0530

    Fixed dependency issue in GetCapsules.js

--- src/GetCapsules.js
+++ src/GetCapsules.js
@@ -7,36 +7,35 @@ function GetCapsules(props) {
   const dispatch = useDispatch();
 
   //geting the capsules
-  const getCapsulesData = async () => {
-    try {
-      const jwt = await createJWT();
-      const API_ENDPOINt = "http://localhost:3300/spacex-capsules/rest/";
-
-      const response = await fetch(API_ENDPOINt, {
-        headers: {
-          'Authorization': jwt
-        }
-      });
-
-      if (!response.ok) {
-        throw new Error(response.statusText);
-      }
-
-      const data = await response.json();
-      console.log("data", data);
-
-
-      dispatch(setCapsules(data));
-
-    } catch (err) {
-      dispatch(setFetchError(err.message));
-      //console.log("error", err.TypeError);
-    }
-  }
-
   useEffect(() => {
-    getCapsulesData();
-  });
+    const fetchCapsules = async () => {
+      try {
+        const jwt = await createJWT();
+        const API_ENDPOINt = "http://localhost:3300/spacex-capsules/rest/";
+  
+        const response = await fetch(API_ENDPOINt, {
+          headers: {
+            'Authorization': jwt
+          }
+        });
+  
+        if (!response.ok) {
+          throw new Error(response.statusText);
+        }
+  
+        const data = await response.json();
+        console.log("data", data);
+  
+        dispatch(setCapsules(data));
+  
+      } catch (err) {
+        dispatch(setFetchError(err.message));
+        //console.log("error", err.TypeError);
+      }
+    }
+    fetchCapsules();
+
+  }, [dispatch]);
 
 }
 
